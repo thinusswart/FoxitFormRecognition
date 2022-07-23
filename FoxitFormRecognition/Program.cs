5ebe6eee4bc837f0cb9b6bcbc2f61278649635ac
Thinus Swart <thinus.swart@gmail.com>
2022-07-23 18:50:02 +0200

    Initial commit

---
+++ FoxitFormRecognition/Program.cs
@@ -0,0 +1,49 @@
+﻿using foxit.common;
+using foxit.pdf;
+using foxit.pdf.interform;
+
+// initialize the Foxit PDF SDK library
+string sn = "<value_from_gsdk_sn.txt>";
+string key = "<value_from_gsdk_key.txt>";
+ErrorCode error_code = Library.Initialize(sn, key);
+if (error_code != ErrorCode.e_ErrSuccess)
+{
+    Library.Release();
+    return;
+}
+
+// load our Sample.pdf file
+PDFDoc doc = new PDFDoc("Sample.pdf");
+error_code = doc.Load(null);
+if (error_code != ErrorCode.e_ErrSuccess)
+{
+    Library.Release();
+    return;
+}
+
+// create a form object that will contain automatically added fields
+using (Form form = new Form(doc))
+{
+    if (form.GetFieldCount("") == 0)
+    {
+        // the progressive class is used for long running tasks like loading documents,
+        // parsing pages and in this case, automatic field recognition
+
+        // start the Form Recognition Engine
+        Progressive pro = doc.StartRecognizeForm(null);
+        Progressive.State state = Progressive.State.e_ToBeContinued;
+
+        // keep looping while the engine is running
+        while (state == Progressive.State.e_ToBeContinued)
+        {
+            state = pro.Continue();
+        }
+    }
+}
+
+// Save the modified PDF to a new file
+string newPdf = "Sample_With_Fields.pdf";
+doc.SaveAs(newPdf, (int)PDFDoc.SaveFlags.e_SaveFlagNoOriginal);
+
+// good practice to release the library when everything is done
+Library.Release();
---
+++ README.md
@@ -0,0 +1,5 @@
+This is a sample application that demonstrates how use the automatic form recognition engine of the [Foxit PDF SDK](https://developers.foxit.com/) to add digital fields to any scanned PDF file.
+
+The application uses a trial version of the Foxit PDF SDK, which is not included in this repository because of licensing.
+
+You can apply for your own trial version of the SDK on [this link here](https://developers.foxit.com/pdf-sdk/free-trial/).
